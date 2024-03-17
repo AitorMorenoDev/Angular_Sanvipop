0e680cda50ddadae81eb4144b31d2168ea7e89e2
Aitor Moreno <119342012+LtVish@users.noreply.github.com>
2024-03-17 10:35:57 +0100

    Updated angular and installed ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,7 @@ import {authInterceptor} from "./interceptors/auth.interceptor";
 import {provideGoogleId} from "./auth/google-login/google-login.config";
 import {provideFacebookId} from "./auth/facebook-login/facebook-login.config";
 import {provideBingmapsKey} from "./profile/bingmaps/bingmaps.config";
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -21,6 +22,6 @@ export const appConfig: ApplicationConfig = {
     provideGoogleId('1066792929235-fm57ku5qv4ncifl4fkvtdsb1j6jvmmsq.apps.googleusercontent.com'),
     provideFacebookId('234349973091670', 'v19.0'),
     provideBingmapsKey('AipGv-br9tFmSkamrlbvhTK-uL1J7nm6PbcUcy73pGQQHMn6x9-_3rBSkmUMJ4Xo'),
-    importProvidersFrom(SweetAlert2Module.forRoot()),
+    importProvidersFrom(SweetAlert2Module.forRoot()), provideClientHydration(),
   ],
 };
